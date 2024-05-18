36997ebc1dec4bdb1b66f4938d7faedae2bbde60
zahashweikh <156355092+zahashweikh@users.noreply.github.com>
2024-05-18 13:12:02 +0300

    Add files via upload

--- Main.js
+++ Main.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".auto-input", {
-    strings: ['a Freelancer', ' Graphic Designer'],
+    strings: ['a 3D Artist'],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
